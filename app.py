c89b42c9e9ca7a6f12f6ea58da0c8df5ac235281
beeerlian <naufal.berlian.99@gmail.com>
2022-06-05 17:29:48 +0700

    fix model load

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
+import base64
+import io
+from urllib import request
 
 import flask
 from keras.models import load_model
+from PIL import Image
+from werkzeug.datastructures import ImmutableMultiDict
 
-from utils import get_model
+from utils import get_model, preprocess_image
 
 app = flask.Flask(__name__)
 
-print('Load model...')
-get_model()
+model = get_model()
 
-
-@app.route("/predict/", methods=['GET', 'POST'])
+@app.route("/predict/", methods=['POST'])
 def predict():
        response = {"success" : True}
-       
-       return response
+       print("Posted file: {}".format(flask.request.files['image']))
+       message = flask.request.files['image']
+       file = message
+       # decoded = base64.b64decode(encoded)
+       # image = Image.open(io.BytesIO(decoded))
+       image = Image.open(file)
+       processed_image = preprocess_image(image, target_size=(416, 416))
+       prediction = model.predict(processed_image).tolist()
+       response['result'] = prediction
+       return flask.jsonify(response)
 
 if __name__ == '__main__' :
        app.run(debug=True, port=8000)
--- utils.py
+++ utils.py
@@ -1,26 +1,23 @@
-import base64
-import io
-
 import numpy as np
 import tensorflow as tf
-from flask import Flask, jsonify, request
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator, img_to_array
-from PIL import Image
+# from PIL import Image
 from tensorflow import keras
 
 print(tf.__version__)
 print(tf.keras.__version__)
 
 def get_model():
-    global model
-    model = load_model('model.h5')
+    print(' * Load model...')
+    model = load_model('model.h5', compile=False)
     print(" * Model loaded!")
+    return model
 
 def preprocess_image(image, target_size):
     if image.mode != "RGB":
         image = image.convert("RGB")
-    image = image.resize(target_size,)
+    image = image.resize(target_size)
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
     return image
